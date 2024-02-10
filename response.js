0cbab494293bed2c6418056b55aeb17905ddeb84
Ran Lachmy <ranlachmy96@gmail.com>
2024-02-10 23:58:35 +0200

    removed file

--- response.js
+++ response.js
@@ -1,37 +1,38 @@
-//Response
-/***************************************************************************************
+// Response
+/** *************************************************************************************
  Function to set the HTTP status code and send a
  response based on the provided code and response data.
- ***************************************************************************************/
+ ************************************************************************************** */
 
-const statusCode = (res, code, response) => {
-
-    switch (code) {
-        case 200 :
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/plain");
-            res.end(JSON.stringify(response));
-            break;
-        case 201 :
-            res.statusCode = 201;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify(response));
-            break;
-        case 400:
-            res.statusCode = 400;
-            res.setHeader("Content-Type", "text/plain");
-            res.end(JSON.stringify({message: response}));
-            break;
-        case 404 :
-            res.statusCode = 404;
-            res.setHeader("Content-Type", "text/plain");
-            res.end(JSON.stringify({message: response}));
-            break;
-        case 500:
-            res.statusCode = 500; // Internal Server Error
-            res.setHeader("Content-Type", "text/plain");
-            res.end(JSON.stringify({message: response}));
-            break;
-    }
-}
-module.exports = {statusCode}
+const responseCode = (res, code, response) => {
+  switch (code) {
+    case 200:
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(JSON.stringify(response));
+      break;
+    case 201:
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(response));
+      break;
+    case 400:
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(JSON.stringify({ message: response }));
+      break;
+    case 404:
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(JSON.stringify({ message: response }));
+      break;
+    case 500:
+      res.statusCode = 500; // Internal Server Error
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(JSON.stringify({ message: response }));
+      break;
+    default:
+      break;
+  }
+};
+module.exports = { responseCode };
