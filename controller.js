db61028049f5d865e7849e612b3a2a29a1373d4c
Ran Lachmy <ranlachmy96@gmail.com>
2024-02-10 03:31:55 +0200

    all CRUD functionality works

--- controller.js
+++ controller.js
@@ -1,134 +1,137 @@
-//controller
-/***************************************************************************************
+// controller
+/** *************************************************************************************
  Importing functions from the repository module and response-related utilities.
- ***************************************************************************************/
+ ************************************************************************************** */
 
 const {
-    getAllReunificationCase,
-    postReunificationCase,
-} = require("./repository");
-const {statusCode} = require("./response")
+  getAllReunificationCase,
+  postReunificationCase,
+} = require('./repository');
+const { statusCode } = require('./response');
 
-/***************************************************************************************
+/** *************************************************************************************
  CHECK functions.
  Function to check the validity of a given ID.
- ***************************************************************************************/
+ ************************************************************************************** */
 
 const checkIdValidity = (id) => {
-    if (typeof id !== 'number') {
-        return false;
-    }
-    const data = getAllReunificationCase();
-    if (data.reunificationCase.some(item => item.id === id)) {
-        return false;
-    }
-}
+  if (typeof id !== 'number') {
+    return false;
+  }
+  const data = getAllReunificationCase();
+  if (data.reunificationCase.some((item) => item.id === id)) {
+    return false;
+  }
+};
 
-/***************************************************************************************
+/** *************************************************************************************
  Middleware for parsing the request body as JSON.
- ***************************************************************************************/
+ ************************************************************************************** */
 
 const bodyParser = (req, res, next) => {
-    let data = '';
-    req.on('data', chunk => {
-        data += chunk;
-    });
-    req.on('end', () => {
-        try {
-            req.body = JSON.parse(data);
-            next();
-        } catch (error) {
-            statusCode(res, 400, "Invalid JSON payload");
-        }
-    });
+  let data = '';
+  req.on('data', (chunk) => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    try {
+      req.body = JSON.parse(data);
+      next();
+    } catch (error) {
+      statusCode(res, 400, 'Invalid JSON payload');
+    }
+  });
 };
 
-/***************************************************************************************
+/** *************************************************************************************
  POST function.
  Function to create a new family reunification case.
- ***************************************************************************************/
+ ************************************************************************************** */
 
 const createReunificationCase = (newReunificationCase, res) => {
-    if (checkIdValidity(newReunificationCase.id) === false) {
-        statusCode(res, 400, "Reunification case Id all ready exists or sent incorrectly");
-    } else {
-        let existingData = getAllReunificationCase();
-        existingData.reunificationCase.push(newReunificationCase);
-        const response = postReunificationCase(existingData);
-        statusCode(res, 201, response);
-    }
+  if (checkIdValidity(newReunificationCase.id) === false) {
+    statusCode(res, 400, 'Reunification case Id all ready exists or sent incorrectly');
+  } else {
+    const existingData = getAllReunificationCase();
+    existingData.reunificationCase.push(newReunificationCase);
+    const response = postReunificationCase(existingData);
+    statusCode(res, 201, response);
+  }
 };
 
-/***************************************************************************************
+/** *************************************************************************************
  GET functions.
  Functions to retrieve all or specific active family reunification cases.
- ***************************************************************************************/
+ ************************************************************************************** */
 
 const readActiveReunificationCase = (req, res) => {
-    const response = getAllReunificationCase();
-    statusCode(res, 200, response);
-}
+  const response = getAllReunificationCase();
+  statusCode(res, 200, response);
+};
 const readReunificationCase = (req, res) => {
-    const url = req.url;
-    const caseId = parseInt(url.split("/")[2]);
-    const response = getAllReunificationCase();
-    const result = response.reunificationCase.find(item => item.id === caseId && item.active === true);
-    if (result) {
-        statusCode(res, 200, result);
-    } else {
-        statusCode(res, 404, "Reunification case not found or not active anymore");
-    }
-}
+  const { url } = req;
+  const caseId = parseInt(url.split('/')[2]);
+  const response = getAllReunificationCase();
+  const result = response.reunificationCase.find((item) => item.id === caseId && item.active === true);
+  if (result) {
+    statusCode(res, 200, result);
+  } else {
+    statusCode(res, 404, 'Reunification case not found or not active anymore');
+  }
+};
 
-/***************************************************************************************
+/** *************************************************************************************
  PUT function.
  Function to update an existing family reunification case.
- ***************************************************************************************/
+ ************************************************************************************** */
 
 const updateReunificationCase = (updatedData, res) => {
-    try {
-        let existingData = getAllReunificationCase();
-        const existingCaseIndex = existingData.reunificationCase.findIndex(item => item.id === updatedData.id);
+  try {
+    const existingData = getAllReunificationCase();
+    const existingCaseIndex = existingData.reunificationCase.findIndex((item) => item.id === updatedData.id);
 
-        if (existingCaseIndex === -1) {
-            statusCode(res, 404, "Reunification case not found");
-        } else {
-            existingData.reunificationCase[existingCaseIndex] = {...existingData.reunificationCase[existingCaseIndex], ...updatedData};
-            const response = postReunificationCase(existingData);
-            statusCode(res, 200, response);
-        }
-    } catch (error) {
-        console.error('Error in updateReunificationCase:', error.message);
-        statusCode(res, 500, "Internal Server Error");
+    if (existingCaseIndex === -1) {
+      statusCode(res, 404, 'Reunification case not found');
+    } else {
+      existingData.reunificationCase[existingCaseIndex] = { ...existingData.reunificationCase[existingCaseIndex], ...updatedData };
+      const response = postReunificationCase(existingData);
+      statusCode(res, 200, response);
     }
+  } catch (error) {
+    console.error('Error in updateReunificationCase:', error.message);
+    statusCode(res, 500, 'Internal Server Error');
+  }
 };
 
-/***************************************************************************************
+/** *************************************************************************************
  DELETE function.
  Function to delete a family reunification case.
- ***************************************************************************************/
+ ************************************************************************************** */
 
 const deleteReunificationCase = (req, res) => {
-    try {
-        const url = req.url;
-        const caseId = parseInt(url.split("/")[2]);
-        const existingData = getAllReunificationCase();
-        const existingCaseIndex = existingData.reunificationCase.findIndex(item => item.id === caseId);
+  try {
+    const { url } = req;
+    const caseId = parseInt(url.split('/')[2]);
+    const existingData = getAllReunificationCase();
+    const existingCaseIndex = existingData.reunificationCase.findIndex((item) => item.id === caseId);
 
-        if (existingCaseIndex === -1) {
-            statusCode(res, 404, "Reunification case not found");
-        } else {
-            existingData.reunificationCase.splice(existingCaseIndex, 1);
-            const response = postReunificationCase(existingData);
-            statusCode(res, 200, response);
-        }
-    } catch (error) {
-        console.error('Error in deleteReunificationCase:', error.message);
-        statusCode(res, 500, "Internal Server Error");
+    if (existingCaseIndex === -1) {
+      statusCode(res, 404, 'Reunification case not found');
+    } else {
+      existingData.reunificationCase.splice(existingCaseIndex, 1);
+      const response = postReunificationCase(existingData);
+      statusCode(res, 200, response);
     }
+  } catch (error) {
+    console.error('Error in deleteReunificationCase:', error.message);
+    statusCode(res, 500, 'Internal Server Error');
+  }
 };
 module.exports = {
-    readActiveReunificationCase, createReunificationCase,
-    updateReunificationCase, deleteReunificationCase, readReunificationCase,
-    bodyParser
-}
+  readActiveReunificationCase,
+  createReunificationCase,
+  updateReunificationCase,
+  deleteReunificationCase,
+  readReunificationCase,
+  bodyParser,
+};
--- router.js
+++ router.js
@@ -1,48 +1,48 @@
-//router
-/******************************************************************************
+// router
+/** ****************************************************************************
  Importing controller functions and response-related utilities.
- *******************************************************************************/
+ ****************************************************************************** */
 
 const {
-    readActiveReunificationCase,
-    deleteReunificationCase,
-    readReunificationCase,
-    bodyParser,
-    createReunificationCase,
-    updateReunificationCase
-} = require("./controller");
-const {statusCode} = require("./response")
+  readActiveReunificationCase,
+  deleteReunificationCase,
+  readReunificationCase,
+  bodyParser,
+  createReunificationCase,
+  updateReunificationCase,
+} = require('./controller');
+const { statusCode } = require('./response');
 
-/******************************************************************************
+/** ****************************************************************************
  Router function that handles incoming requests
  and routes them to the appropriate controller function.
- *******************************************************************************/
+ ****************************************************************************** */
 
 const router = (req, res) => {
-    const {method, url} = req;
-    const parts = url.split('/');
-    const route = parts[1];
-    if (method === 'GET' && route === 'familyReunification' && parts.length === 2) {
-        readActiveReunificationCase(req, res);
-    } else if (method === 'GET' && route === 'familyReunification' && parts.length >= 3) {
-        readReunificationCase(req, res);
-    } else if ((method === 'POST' || method === 'PUT') && route === 'familyReunification') {
-        bodyParser(req, res, () => {
-            const requestData = req.body;
-            switch (method) {
-                case 'POST':
-                    createReunificationCase(requestData, res);
-                    break;
-                case 'PUT':
-                    updateReunificationCase(requestData, res);
-                    break;
-            }
-        });
-    } else if (method === 'DELETE' && route === 'familyReunification') {
-        deleteReunificationCase(req, res);
-    } else {
-        statusCode(res, 404, "Invalid route");
-    }
+  const { method, url } = req;
+  const parts = url.split('/');
+  const route = parts[1];
+  if (method === 'GET' && route === 'familyReunification' && parts.length === 2) {
+    readActiveReunificationCase(req, res);
+  } else if (method === 'GET' && route === 'familyReunification' && parts.length >= 3) {
+    readReunificationCase(req, res);
+  } else if ((method === 'POST' || method === 'PUT') && route === 'familyReunification') {
+    bodyParser(req, res, () => {
+      const requestData = req.body;
+      switch (method) {
+        case 'POST':
+          createReunificationCase(requestData, res);
+          break;
+        case 'PUT':
+          updateReunificationCase(requestData, res);
+          break;
+      }
+    });
+  } else if (method === 'DELETE' && route === 'familyReunification') {
+    deleteReunificationCase(req, res);
+  } else {
+    statusCode(res, 404, 'Invalid route');
+  }
 };
 
-module.exports = {router}
+module.exports = { router };
